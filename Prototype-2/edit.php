48f0e3bc2601fc664bed607d30e9e20f3899c3ce
ELGHAZY2007 <elghazy.mohamedreda@gmail.com>
2022-02-21 12:17:52 +0100

    up

---
+++ Prototype-2/edit.php
@@ -0,0 +1,45 @@
+<?php  
+include 'config.php';
+
+    if(isset($_GET['id'])){
+        $id = $_GET['id'];
+       
+        $Select = "SELECT * FROM person WHERE id=$id";
+        // get result
+        $result = mysqli_query($conn, $Select);
+
+         // fetch to array
+        $person = mysqli_fetch_assoc($result);
+
+    }
+
+    if(!empty($_POST)){
+        
+        $prenom = $_POST['Prenom'];
+        $nom = $_POST['Nom'];
+        $age = $_POST['Age'];
+
+        $update ="UPDATE person SET 
+        prénom='$prenom',nom='$nom',age='$age'";
+
+        mysqli_query($conn, $update);
+        header('location: index.php');
+        
+    };
+    
+?>
+<form action="" method="post">
+    <div>
+        <input type="text"  value="<?= $person['prénom'] ?>" name="Prenom">
+    </div>
+    <div>
+    <input type="text"  value="<?= $person['nom'] ?>"name="Nom">
+     </div>
+    <div>	
+        <input type="number" value="<?= $person['age'] ?>" name="Age">
+    </div>
+	<div>
+        <button type='submit'>Modifier</button>
+    </div>
+	
+</form>
