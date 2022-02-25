c32fcddb538713f778773c270a05a3d31501645a
ELGHAZY2007 <elghazy.mohamedreda@gmail.com>
2022-02-25 15:48:04 +0100

    up

--- Prototype-3/config.php
+++ Prototype-3/config.php
@@ -1,8 +1,8 @@
 <?php
-   $conn = mysqli_connect('localhost', 'test', 'test123', 'demo2');
+   $connect = mysqli_connect('localhost', 'REDA', 'Halhoulidu7373', 'demo');
 
    // check connection
- if(!$conn){
+ if(!$connect){
       echo 'Connection error: ' . mysqli_connect_error(); 
   }
 ?>
