4a25145551ff48b2fa879239fab8a718a6b8badf
ELGHAZY2007 <elghazy.mohamedreda@gmail.com>
2022-03-03 15:18:17 +0100

    update Prototype-0

--- Prototype-0/index.php
+++ Prototype-0/index.php
@@ -1,1 +1,43 @@
-<
+<?php
+
+$ajouterFicher = file_get_contents("personnes.json");
+$data = json_decode($ajouterFicher);
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <div>
+        <a href="insert.php">ajoute</a>    
+    </div>
+    <table>
+    <tr>
+                <th>Prenom</th>
+                <th>Nom</th>
+                <th>Age</th>
+            </tr>
+ <?php
+  foreach($data as $value){
+
+ ?>
+ <tr>
+    <td><?php echo $value[1] ?></td>
+    <td><?php echo $value[2]?></td>
+    <td><?php echo $value[3]?></td>
+ <td>
+
+ </td>
+ </tr>
+
+<?php } ?>
+ </table>
+</body>
+</html>
--- Prototype-0/insert.php
+++ Prototype-0/insert.php
@@ -1,59 +1,40 @@
-<?php 
-    if ( !empty($_POST)) { 
-        // post values
-        $fname  = $_POST['fname'];
-        $lname  = $_POST['lname'];
-        $age    = $_POST['age'];
-        $gender = $_POST['gender'];
-		$person = array($fname, $lname, $age, $gender); 
-      
-		$file = file_get_contents('people.json');
-		$data = json_decode($file, true);
-		// unset($_POST["add"]);
-		// $data["records"] = array_values($data["records"]);
-		array_push($data, $person);
-		file_put_contents("people.json", json_encode($data));
-		header("Location: index.php");
+<?php
 
-    }
+if(!empty($_POST)){
+	$id =uniqid(false);
+	$Prenom = $_POST['prenom'];
+	$Nom = $_POST['nom'];
+	$Age = $_POST['age'];
+	$person = array($id,$Prenom,$Nom,$Age);
+	$fichier = file_get_contents("personnes.json");
+	$data = json_decode($fichier);
+	array_push($data,$person);	
+	
+	file_put_contents("personnes.json",json_encode($data))	;
+	header("location: index.php"); 
+
+}
+
+
 ?>
-<div>
-        <div>
-		<div><h3>Create a User</h3>
-        <form method="POST" action="">
-			<div>
-				<label for="inputFName">First Name</label>
-				<input type="text" required="required" id="inputFName" name="fname" placeholder="First Name">
-				<span></span>
-			</div>
-			
-			<div>
-				<label for="inputLName">Last Name</label>
-				<input type="text" required="required" id="inputLName" name="lname" placeholder="Last Name">
-        		<span></span>
-			</div>
-			
-			<div>
-				<label for="inputAge">Age</label>
-				<input type="number" required="required" class="form-control" id="inputAge" name="age" placeholder="Age">
-				<span></span>
-			</div>
-				<div class="form-group">
-					<label for="inputGender">Gender</label>
-					<select class="form-control" required="required" id="inputGender" name="gender" >
-						<option>Please Select</option>
-						<option value="Male">Male</option>
-						<option value="Female">Female</option>
-					</select>
-					<span></span>
-        		</div>
-    
-			<div class="form-actions">
-					<button type="submit">Create</button>
-					<a href="index.php">Back</a>
-			</div>
-		</form>
-        </div></div>        
-</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+	
+<form action="" method='post'>
+	
+Prenom : <input type="text" name="prenom">
+Nom : <input type="text" name="nom">
+Age : <input type="text" name="age">
+
+<button type='submit'>ajoute</button>
+</form>
+
 </body>
 </html>
